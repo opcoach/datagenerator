882fb2828c588f6d406881843d5c9cf1902b9d84
Olivier Prouvost <olivier.prouvost@opcoach.com>
2017-11-27 16:48:00 +0100

    Generate interfaces with genmodeladdon 2.0
    Change-Id: I5a2439663618134391f1f98fc8e60ed64f9bb53c
    Signed-off-by: Olivier Prouvost <olivier.prouvost@opcoach.com>

--- com.opcoach.generator.model/src/com/opcoach/generator/RangeGenerator.java
+++ com.opcoach.generator.model/src/com/opcoach/generator/RangeGenerator.java
@@ -3,5 +3,10 @@ package com.opcoach.generator;
 // This interface overrides the generated interface and will be returned by factory
 public interface RangeGenerator<T> extends MRangeGenerator<T>
 {
+	// You can write additional methods using an empty default java 8 notation 
+	// because the generated implemented class extends this interface and is not abstract
+	//
+	// For instance : 
+	// default public void addSomething(Something foo) {} ;
 
 }
--- com.opcoach.generator.model/src/com/opcoach/generator/basic/DateGenerator.java
+++ com.opcoach.generator.model/src/com/opcoach/generator/basic/DateGenerator.java
@@ -3,5 +3,10 @@ package com.opcoach.generator.basic;
 // This interface overrides the generated interface and will be returned by factory
 public interface DateGenerator extends MDateGenerator
 {
+	// You can write additional methods using an empty default java 8 notation 
+	// because the generated implemented class extends this interface and is not abstract
+	//
+	// For instance : 
+	// default public void addSomething(Something foo) {} ;
 
 }
